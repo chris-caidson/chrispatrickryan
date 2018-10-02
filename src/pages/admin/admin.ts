33e5bcb9c21c4720810ab60b8ba047c50c40c728
chris-caidson <chris@caidson.com>
2018-10-02 07:49:01 -0400

    Implementing authentication.

--- src/pages/admin/admin.ts
+++ src/pages/admin/admin.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AuthProvider } from '../../providers/auth/auth';
 
 @IonicPage()
 @Component({
@@ -7,8 +8,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'admin.html',
 })
 export class AdminPage {
+  public adminPassword: string = "";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public auth: AuthProvider) {
   }
 
+  public login() {
+    this.auth.login(this.adminPassword);
+  }
+
+  public logout() {
+    this.auth.logout();
+  }
 }
--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,11 +1,36 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import * as firebase from "firebase";
+import { firebaseConfig } from "../../app/app.module";
 
 @Injectable()
 export class AuthProvider {
+  private storageKey: string = "chrisPatrickRyanLoggedIn";
 
-  constructor(public http: HttpClient) {
-    
+  incorrectPassword: boolean = false;
+  loggedIn: boolean = localStorage.getItem(this.storageKey) != null;
+
+  constructor() {
+    firebase.initializeApp(firebaseConfig);
   }
 
+  public login(password: string) {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword("admin@chrispatrickryan.com", password)
+      .then(() => {
+        this.loggedIn = true;
+        this.incorrectPassword = false;
+        localStorage.setItem(this.storageKey, "true");
+      })
+      .catch(() => {
+        this.loggedIn = false;
+        this.incorrectPassword = true;
+      });
+  }
+
+  public logout() {
+    firebase.auth().signOut();
+    this.loggedIn = false;
+    localStorage.removeItem(this.storageKey);
+  }
 }
